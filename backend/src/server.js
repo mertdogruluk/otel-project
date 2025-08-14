47e3456c40d771bda6df15267879bc30b8ee4e1c
DİLEK <dilekayvaz0@gmail.com>
2025-08-14 17:38:40 +0300

    sunucu port numarası 5000'den 3000'e güncellendi.

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server ${PORT} portunda çalışıyor`);
