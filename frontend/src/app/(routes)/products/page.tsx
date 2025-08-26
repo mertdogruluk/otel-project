896b3de70127630a6d5493c70c62d7f70fcab9bc
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2025-08-26 09:58:13 +0300

    feat(product-page): add hero section

--- frontend/src/app/(routes)/products/page.tsx
+++ frontend/src/app/(routes)/products/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import HotelFilters from "@/components/filters/HotelFilters";
+import Pagination from "@/components/filters/Pagination";
+import DatePicker from "@/components/forms/DatePicker";
+import HotelsList from "@/components/hotels/HotelsList";
+import Image from "next/image";
+import React from "react";
 
 function ProductPage() {
   return (
-    <div>ProductPage</div>
-  )
+    <>
+      {/* Hero Section */}
+      <div className="flex justify-center items-center my-36">
+        <div className="relative">
+          {/* Background Image */}
+          <div className="absolute inset-0 flex justify-center items-center -z-10">
+            <Image
+              src="/images/mappa-product.png"
+              alt="World Map Background"
+              width={802}
+              height={355}
+              className="object-contain"
+            />
+          </div>
+
+          {/* DatePicker Component */}
+          <DatePicker />
+        </div>
+      </div>
+
+      {/* Hotel Filters and Hotels List Section */}
+      <div className="flex flex-row justify-between gap-4">
+        {/* Hotel Filters */}
+        <div>
+          <HotelFilters />
+        </div>
+
+        {/* Hotels List and Pagination */}
+        <div className="flex flex-col gap-4">
+          <HotelsList />
+          <Pagination />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ProductPage;
